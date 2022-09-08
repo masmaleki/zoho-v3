c12fae5ca0995232435508fe02643ec4cb31daae
Shahriar Edalat Zadeh <shahriared@gmail.com>
2022-09-08 19:17:53 +0400

    feat: add create and update contact methods.

--- src/ZohoAllInOne.php
+++ src/ZohoAllInOne.php
@@ -30,5 +30,15 @@ class ZohoAllInOne
     {
         return ZohoContactController::getByEmail($zoho_email);
     }
+
+    public static function createContact($data = [])
+    {
+        return ZohoContactController::create($data);
+    }
+
+    public static function updateContact($zoho_contact_id, $data = [])
+    {
+        return ZohoContactController::updateById($zoho_contact_id, $data);
+    }
     // end - contact functions
 }
