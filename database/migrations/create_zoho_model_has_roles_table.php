385bcbf95c72200aca6e94311435403670c35d9c
Shahriar Edalat Zadeh <shahriared@gmail.com>
2022-09-13 11:27:08 +0400

    feat: add zoho model has role migration and seeders.

---
+++ database/migrations/create_zoho_model_has_roles_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::create('zoho_model_has_roles', function (Blueprint $table) {
+            $table->unsignedBigInteger('role_id');
+            $table->string('model_type');
+            $table->unsignedBigInteger('model_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('zoho_model_has_roles');
+    }
+};
---
+++ database/seeders/ZohoUserHasRoleSeeder.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Masmaleki\ZohoAllInOne\Models\ZohoModelHasRole;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Hash;
+
+class ZohoUserHasRoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::statement("SET foreign_key_checks=0");
+        ZohoModelHasRole::truncate();
+
+        foreach (config('zoho-v3.roles') as $key => $role) {
+            $user = User::where('email', $role['default_email'])->first();
+            if (!$user) {
+                $user = new User();
+                $user->name = $role['name'];
+                $user->email = $role['default_email'];
+                $user->password = Hash::make($role['default_email']);
+                $user->email_verified_at = now();
+                $user->remember_token = Str::random(10);
+                $user->save();
+            }
+
+            $zohoModelHasRole = new ZohoModelHasRole();
+            $zohoModelHasRole->role_id = $key;
+            $zohoModelHasRole->model_type = User::class;
+            $zohoModelHasRole->model_id = $user->id;
+            $zohoModelHasRole->save();
+        }
+
+        DB::statement("SET foreign_key_checks=1");
+    }
+}
