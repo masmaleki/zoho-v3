1481b461a84879d56b0ef4a65c8f19b4873e89a1
Ali Mehraei <ali.mehraei@gmail.com>
2022-09-28 10:45:22 +0330

    feat: add zoho manufacture controller.

---
+++ src/Http/Controllers/Records/ZohoManufactureController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Masmaleki\ZohoAllInOne\Http\Controllers\Records;
+
+
+use GuzzleHttp\Client;
+use Masmaleki\ZohoAllInOne\Http\Controllers\Auth\ZohoTokenCheck;
+
+class ZohoManufactureController
+{
+
+    public static function getAll($page_token = null)
+    {
+        $token = ZohoTokenCheck::getToken();
+        if (!$token) {
+            return null;
+        }
+        $apiURL = $token->api_domain . '/crm/v3/' . config('zoho-v3.custom_modules_names.manufacture') . '?fields=$review_process,$field_states,Record_Image,$approval_state,Email,Active,Name,Octo_API_Id,$approval,Vendor_Strong_Lines';
+        if ($page_token) {
+            $apiURL .= '&page_token=' . $page_token;
+        }
+        $client = new Client();
+
+        $headers = [
+            'Authorization' => 'Zoho-oauthtoken ' . $token->access_token,
+        ];
+
+        $response = $client->request('GET', $apiURL, ['headers' => $headers]);
+        $statusCode = $response->getStatusCode();
+        $responseBody = json_decode($response->getBody(), true);
+        dd($responseBody);
+        return $responseBody;
+    }
+
+}
