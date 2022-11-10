ad6cba9f9cb332811a8701e816f6b74b33e9ad52
Shahriar Edalat Zadeh <shahriared@gmail.com>
2022-11-10 14:07:06 +0400

    feat: add zoho crm organization controller.

---
+++ src/Http/Controllers/Settings/ZohoCrmOrganizationController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Masmaleki\ZohoAllInOne\Http\Controllers\Settings;
+
+use GuzzleHttp\Client;
+use Masmaleki\ZohoAllInOne\Http\Controllers\Auth\ZohoTokenCheck;
+
+class ZohoCrmOrganizationController
+{
+
+    public static function get()
+    {
+        $token = ZohoTokenCheck::getToken();
+        if (!$token) {
+            return null;
+        }
+        $apiURL = $token->api_domain . '/crm/v3/org';
+        $client = new Client();
+
+        $headers = [
+            'Authorization' => 'Zoho-oauthtoken ' . $token->access_token,
+        ];
+
+        $response = $client->request('GET', $apiURL, ['headers' => $headers]);
+        $statusCode = $response->getStatusCode();
+        $responseBody = json_decode($response->getBody(), true);
+        return $responseBody;
+    }
+
+
+}
