f1286bfa227bf3bdeaa2d61ea22aedd0f9401eec
Mohammad Sadegh Maleki <masmaleki@gmail.com>
2022-11-25 20:48:28 +0400

    Feat::Add New Model and Controller for merging duplicated products purpose

---
+++ src/Http/Controllers/Records/ZohoAllInOneObjectController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Masmaleki\ZohoAllInOne\Http\Controllers\Records;
+
+use App\Http\Controllers\Controller;
+
+class ZohoAllInOneObjectController extends Controller
+{
+    //
+}
---
+++ src/Models/AllInOneObject.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Masmaleki\ZohoAllInOne\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class AllInOneObject extends Model
+{
+    use HasFactory;
+}
