22b2a3c300339d76f68e24fee5d854322cbbdd12
Ali Mehraei <ali.mehraei@gmail.com>
2022-11-28 22:52:46 +0330

    feat: update zoho all in one object model.

--- src/Models/AllInOneObject.php
+++ src/Models/AllInOneObject.php
@@ -9,5 +9,25 @@ class AllInOneObject extends Model
 {
     protected $guarded = [];
 
-    use HasFactory;
+    public function __construct(array $attributes = [])
+    {
+        parent::__construct($attributes);
+        $this->is_main = false;
+        $this->mark_as_deleted = false;
+
+        $this->crm_objects_count = 0;
+        $this->books_objects_count = 0;
+
+        //$this->processes = false;
+
+        foreach (['rfq', 'quotes', 'availabilities'] as $crmItem) {
+            $this->crm_objects_count += $this->$crmItem['count'];
+        }
+        if ($this->books_id) {
+            foreach (['invoices', 'pos', 'sos'] as $bookItem) {
+                $this->books_objects_count += $this->$bookItem['count'];
+            }
+        }
+    }
+
 }
