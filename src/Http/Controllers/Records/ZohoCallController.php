b19727cf4869ca6e37fc87b6bf76f325d6c4459a
Ali Mehraei <ali.mehraei@gmail.com>
2022-12-14 12:01:51 +0330

    feat: add zoho call controller.

---
+++ src/Http/Controllers/Records/ZohoCallController.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace Masmaleki\ZohoAllInOne\Http\Controllers\Records;
+
+use GuzzleHttp\Client;
+use Masmaleki\ZohoAllInOne\Http\Controllers\Auth\ZohoTokenCheck;
+
+class ZohoCallController
+{
+
+    public static function get($call_id)
+    {
+        $token = ZohoTokenCheck::getToken();
+        if (!$token) {
+            return null;
+        }
+        $apiURL = $token->api_domain . '/crm/v3/Calls/' . $call_id;
+
+        $client = new Client();
+
+        $headers = [
+            'Authorization' => 'Zoho-oauthtoken ' . $token->access_token,
+        ];
+
+        $response = $client->request('GET', $apiURL, ['headers' => $headers]);
+        $statusCode = $response->getStatusCode();
+        $responseBody = json_decode($response->getBody(), true);
+        return $responseBody;
+    }
+
+    public static function getAll($page_token = null, $fields = null)
+    {
+        $token = ZohoTokenCheck::getToken();
+        if (!$token) {
+            return null;
+        }
+        $fields = $fields ? $fields : 'Subject,What_Id,Owner,Who_Id,id,Priority,Status,Due_Date,Modified_Time,Closed_Time,Remind_At,Description';
+        $apiURL = $token->api_domain . '/crm/v3/Calls?fields=' . $fields;
+        if ($page_token) {
+            $apiURL .= '&page_token=' . $page_token;
+        }
+        $client = new Client();
+
+        $headers = [
+            'Authorization' => 'Zoho-oauthtoken ' . $token->access_token,
+        ];
+
+        $response = $client->request('GET', $apiURL, ['headers' => $headers]);
+        $statusCode = $response->getStatusCode();
+        $responseBody = json_decode($response->getBody(), true);
+        return $responseBody;
+    }
+
+    public static function getByCOQL($conditions = null, $offset = 0, $fields = null)
+    {
+        $token = ZohoTokenCheck::getToken();
+        if (!$token) {
+            return null;
+        }
+
+        $apiURL = $token->api_domain . '/crm/v3/coql';
+        $client = new Client();
+
+        $headers = [
+            'Authorization' => 'Zoho-oauthtoken ' . $token->access_token,
+        ];
+
+        $fields = $fields ? $fields : ' Call_Agenda, Call_Duration, Call_Purpose, Call_Result, id, Call_Start_Time, Call_Type, Caller_ID, Who_Id, Created_By, Description, Dialled_Number , Modified_By , Outgoing_Call_Status , What_Id , Scheduled_In_CRM ,Contact ';
+       // $fields = $fields ? $fields : ' Subject, What_Id, Owner, Owner, Who_Id, id, $se_module, Priority, Status, Due_Date, Modified_Time, Closed_Time, Remind_At ';
+        $conditions = $conditions ? $conditions : " (id != 0) ";
+        $body = [
+            'select_query' => "select " . $fields . " from Calls where " . $conditions . "  limit " . $offset . ", 200",
+        ];
+
+        $response = $client->request('POST', $apiURL, ['headers' => $headers, 'body' => json_encode($body)]);
+
+        $statusCode = $response->getStatusCode();
+        $responseBody = json_decode($response->getBody(), true);
+        return $responseBody;
+    }
+
+    public static function search($phrase, $criteria = null)
+    {
+        $token = ZohoTokenCheck::getToken();
+        if (!$token) {
+            return null;
+        }
+        $apiURL = $token->api_domain . '/crm/v3/Calls/search?word=' . $phrase . $criteria;
+        $client = new Client();
+
+        $headers = [
+            'Authorization' => 'Zoho-oauthtoken ' . $token->access_token,
+        ];
+
+        $response = $client->request('GET', $apiURL, ['headers' => $headers]);
+        $statusCode = $response->getStatusCode();
+        $responseBody = json_decode($response->getBody(), true);
+        return $responseBody;
+    }
+
+    public static function create($data = null)
+    {
+        if (!$data) {
+            return null;
+        }
+        $token = ZohoTokenCheck::getToken();
+        if (!$token) {
+            return null;
+        }
+        $apiURL = $token->api_domain . '/crm/v3/Calls';
+        $client = new Client();
+
+        $headers = [
+            'Authorization' => 'Zoho-oauthtoken ' . $token->access_token,
+        ];
+
+        $body = [
+            'data' => [
+                0 => $data
+            ]
+        ];
+        $response = $client->request('POST', $apiURL, ['headers' => $headers, 'body' => json_encode($body)]);
+        $statusCode = $response->getStatusCode();
+        $responseBody = json_decode($response->getBody(), true);
+        return $responseBody;
+    }
+
+}
