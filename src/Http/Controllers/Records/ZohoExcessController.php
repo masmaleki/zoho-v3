e42c689f6b770fc79d3ef6b9b5a0d58ff87f298a
Shahriar <shahriared@gmail.com>
2023-04-21 08:31:54 +0400

    feat: add zoho excess controller.

---
+++ src/Http/Controllers/Records/ZohoExcessController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Masmaleki\ZohoAllInOne\Http\Controllers\Records;
+
+use GuzzleHttp\Client;
+use Masmaleki\ZohoAllInOne\Http\Controllers\Auth\ZohoTokenCheck;
+
+class ZohoExcessController
+{
+    public static function create($data = null)
+    {
+        try{
+        if (!$data) {
+            return null;
+        }
+        $token = ZohoTokenCheck::getToken();
+        if (!$token) {
+            return null;
+        }
+        $apiURL = $token->api_domain . '/crm/v3/' . config('zoho-v3.custom_modules_names.excess');
+        $client = new Client();
+
+        $headers = [
+            'Authorization' => 'Zoho-oauthtoken ' . $token->access_token,
+        ];
+
+        $body = [
+            'data' => [
+                0 => $data
+            ]
+        ];
+        $response = $client->request('POST', $apiURL, ['headers' => $headers, 'body' => json_encode($body)]);
+        $statusCode = $response->getStatusCode();
+        $responseBody = json_decode($response->getBody(), true);
+        return $responseBody;
+    } catch (\GuzzleHttp\Exception\ClientException $e) {
+
+        dd($e->getResponse()->getBody()->getContents(), $data);
+
+    }
+    }
+}
