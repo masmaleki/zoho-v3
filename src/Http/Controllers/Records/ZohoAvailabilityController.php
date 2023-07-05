4753997c547168daa95469873693a730d79b83c2
Shahriar <shahriared@gmail.com>
2023-07-05 07:27:36 +0400

    feat: add create AVA function.

--- src/Http/Controllers/Records/ZohoAvailabilityController.php
+++ src/Http/Controllers/Records/ZohoAvailabilityController.php
@@ -100,29 +100,29 @@ class ZohoAvailabilityController
 
     public static function create($data = null)
     {
-//        if (!$data) {
-//            return null;
-//        }
-//        $token = ZohoTokenCheck::getToken();
-//        if (!$token) {
-//            return null;
-//        }
-//        $apiURL = $token->api_domain . '/crm/v3/Availability' ;
-//        $client = new Client();
-//
-//        $headers = [
-//            'Authorization' => 'Zoho-oauthtoken ' . $token->access_token,
-//        ];
-//
-//        $body = [
-//            'data' => [
-//                0 => $data
-//            ]
-//        ];
-//        $response = $client->request('POST', $apiURL, ['headers' => $headers, 'body' => json_encode($body)]);
-//        $statusCode = $response->getStatusCode();
-//        $responseBody = json_decode($response->getBody(), true);
-//        return $responseBody;
+        if (!$data) {
+            return null;
+        }
+        $token = ZohoTokenCheck::getToken();
+        if (!$token) {
+            return null;
+        }
+        $apiURL = $token->api_domain . '/crm/v3/Availability';
+        $client = new Client();
+
+        $headers = [
+            'Authorization' => 'Zoho-oauthtoken ' . $token->access_token,
+        ];
+
+        $body = [
+            'data' => [
+                0 => $data
+            ]
+        ];
+        $response = $client->request('POST', $apiURL, ['headers' => $headers, 'body' => json_encode($body)]);
+        $statusCode = $response->getStatusCode();
+        $responseBody = json_decode($response->getBody(), true);
+        return $responseBody;
     }
 
     public static function update($data = [])
