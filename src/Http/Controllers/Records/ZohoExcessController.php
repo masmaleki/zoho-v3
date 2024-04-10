c54220035df738781b6428ff28ae86ae66be059d
davood.thinker <davood.thinker@gmail.com>
2024-04-10 13:55:24 +0330

    check update excess

--- src/Http/Controllers/Records/ZohoExcessController.php
+++ src/Http/Controllers/Records/ZohoExcessController.php
@@ -178,13 +178,13 @@ class ZohoExcessController
             $todayEnd = Carbon::today()->addDay()->format("Y-m-d") . "T23:59:59+00:00";
 
             if ($action == 'create') {
-                $condition = "sync_with_panel is null and Created_Time between '{$todayStart}' and '{$todayEnd}'";
+                $condition = "sync_with_panel is null and Modified_Time between '{$todayStart}' and '{$todayEnd}'";
             } else {
-                $condition = "Modified_Time between '{$todayStart}' and '{$todayEnd}' and sync_panel <> Modified_Time";
+                $condition = "(sync_with_panel is not null) and (sync_with_panel < Modified_Time) and (Modified_Time between '{$todayStart}' and '{$todayEnd}')";
             }
         }
         $body = [
-            'select_query' => "select " . $fields . " from " . config('zoho-v4.custom_modules_names.excess') . " where " . $condition . " order by Created_Time desc limit " . $offset . ", 200",
+            'select_query' => "select " . $fields . " from " . config('zoho-v4.custom_modules_names.excess') . " where " . $condition . " order by Modified_Time desc limit " . $offset . ", 200",
         ];
         // dd($body);
         $response = $client->request('POST', $apiURL, ['headers' => $headers, 'body' => json_encode($body)]);
