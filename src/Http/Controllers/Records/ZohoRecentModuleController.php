75cf2be3ea3310a80519a0441ce3b42578218703
Ali Mehraei <ali.mehraei@gmail.com>
2024-06-25 09:28:47 +0330

    feat: improve the zoho recent module controller get function.

--- src/Http/Controllers/Records/ZohoRecentModuleController.php
+++ src/Http/Controllers/Records/ZohoRecentModuleController.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Carbon;
 class ZohoRecentModuleController
 {
 
-    public static function getRecentModuleIdFieldV6($module, $action = 'create')
+    public static function getRecentModuleIdFieldV6($module, $action = 'create', $offset = 0, $perPage = 200, $fields = null)
     {
         $token = ZohoTokenCheck::getToken();
         if (!$token) {
@@ -30,8 +30,7 @@ class ZohoRecentModuleController
             'Authorization' => 'Zoho-oauthtoken ' . $token->access_token,
         ];
 
-        $fields = 'id';
-
+        $fields = $fields ?: 'id';
 
         $startDay = Carbon::today()->subDays(config('zoho-v4.sync_new_records_period_in_days'))->format("Y-m-d") . "T00:00:01+00:00";
         $endDay = Carbon::today()->addDay(1)->format("Y-m-d") . "T23:59:59+00:00";
@@ -39,19 +38,20 @@ class ZohoRecentModuleController
         switch ($action) {
             case 'create':
                 $condition = "Sales_Tools_Synced_At is null and Modified_Time between '{$startDay}' and '{$endDay}'";
+                $query = "select " . $fields . " from " . $module . " where " . $condition . " order by Modified_Time desc limit " . $offset . ", " . $perPage;
                 break;
             case 'edit':
                 $condition = "Sales_Tools_Synced_At is not null and Modified_Time between '{$startDay}' and '{$endDay}'";
+                $query = "select " . $fields . " from " . $module . " where " . $condition . " order by Sales_Tools_Synced_At desc limit " . $offset . ", " . $perPage;
                 break;
             case 'sync':
                 $condition = "Created_Time between '{$startDay}' and '{$endDay}'";
+                $query = "select " . $fields . " from " . $module . " where " . $condition . " order by Created_Time desc limit " . $offset . ", " . $perPage;
                 break;
         }
 
 
-        $body = [
-            'select_query' => "select " . $fields . " from " . $module . " where " . $condition . " order by Modified_Time desc limit 0, 200",
-        ];
+        $body = ['select_query' => $query,];
 
         try {
             $response = $client->request('POST', $apiURL, ['headers' => $headers, 'body' => json_encode($body)]);
